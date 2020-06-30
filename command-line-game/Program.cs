ae76dba5e9d33ca955362ae14b937a719200ca87
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2020-06-30 20:12:43 +0200

    Test2

--- command-line-game/Program.cs
+++ command-line-game/Program.cs
@@ -6,7 +6,22 @@ namespace command_line_game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!!");
+            string A = "1";
+            string B = "2";
+            string C = "3";
+            string D = "4";
+            string E = "5";
+            string F = "6";
+            string G = "7";
+            string H = "8";
+            string I = "9";
+
+            Console.Title = "Kółko i krzyżyk";
+            Console.WriteLine("Witaj w grze kółko i krzyżyk!");
+            Console.WriteLine("Wciśnij jakiś klawisz... ");
+            Console.ReadLine();
+            Console.Clear();
+
         }
     }
 }
