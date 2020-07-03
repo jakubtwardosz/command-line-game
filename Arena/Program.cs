10323e56bb7efd9dcacbc5ea28b9e7953e99e1f3
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2020-07-03 10:11:56 +0200

    I started editing the main program file

--- Arena/Program.cs
+++ Arena/Program.cs
@@ -8,14 +8,53 @@ namespace Arena
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Arena!");
+            
+            
 
-            Warrior maximus = new Warrior("maximus", 1000, 120, 40);
+            
 
-            Warrior bob = new Warrior("bob", 1000, 120, 40);
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Your result:");
+                    break;
+                case "a":
+                    Console.WriteLine($"Your result:");
+                    break;
+                case "a":
+                    Console.WriteLine($"Your result:");
+                    break;
+                case "a":
+                    Console.WriteLine($"Your result:");
+                    break;
 
-            Battle.StartFight(maximus, bob);
+            }
 
-            Console.ReadLine();
+
+
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine("Avalible actions:");
+            Console.WriteLine("\t1 - Choose a difficulty level");
+            Console.WriteLine("\t2 - Check your items");
+            Console.WriteLine("\t3 - Check the level of experience");
+            Console.WriteLine("\t4 - Start fighting!");
+            Console.WriteLine("\t5 - Hall of Fame");
+            Console.Write("Choose action: ");
+        }
+
+        private static NewPlayer()
+        {
+            Console.WriteLine("Enter the name of your warrior: ");
+
+            Warrior player = new Warrior(Console.ReadLine());
+
+            Console.WriteLine("Hello, {0}!", player.Name);
+
+            return player;
+
         }
     }
 }
--- ArenaLibrary/Battle.cs
+++ ArenaLibrary/Battle.cs
@@ -4,6 +4,65 @@ namespace ArenaLibrary
 {
     public class Battle
     {
+        // Start Fight
+        // WarriorA WarriorB
+        public static void StartFight(Warrior warriorA, Warrior warriorB)
+        {
+
+            // Loop giving warrior a chance to attack and block each turn until 1 dies
+
+            while (true)
+            {
+                if (GetAttackResult(warriorA, warriorB) == "Game Over")
+                {
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+
+                if (GetAttackResult(warriorB, warriorA) == "Game Over")
+                {
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+            }
+
+        }
+        // GetAttackResult
+        // Warrior1, Warriotr2
+
+        public static string GetAttackResult(Warrior warrior1, Warrior warrior2)
+        {
+            double warrior1AttAmt = warrior1.Attack();
+            double warrior2BlkAmt = warrior2.Block();
+
+            double dmgToWarrior2 = warrior1AttAmt - warrior2BlkAmt;
+
+            if (dmgToWarrior2 > 0)
+            {
+                warrior2.Health = warrior2.Health - dmgToWarrior2;
+            }
+            else dmgToWarrior2 = 0;
+
+            Console.WriteLine("{0} Attacks {1} and Deals {2} Damage", warrior1.Name, warrior2.Name, dmgToWarrior2);
+
+            Console.WriteLine("{0} Has {1} Health \n", warrior2.Name, warrior2.Health);
+
+            // death
+            if (warrior2.Health <= 0)
+            {
+                Console.WriteLine("{0} has Died and {1} is victorius \n", warrior2.Name, warrior1.Name);
+                return "Game Over";
+            }
+            else return "Fight again";
+
+        }
+
+
+
+
+        
+
+
         
     }
 }
