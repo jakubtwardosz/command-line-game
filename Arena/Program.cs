e68d50027e999a2837022956f76c4d5c153cffc1
Jakub Twardosz <jakub.twardosz@microsoft.wsei.edu.pl>
2020-07-12 21:14:41 +0200

    Next changes

--- Arena/Program.cs
+++ Arena/Program.cs
@@ -8,12 +8,60 @@ namespace Arena
     {
         static void Main(string[] args)
         {
-            Player hero = new Player(0,"Warrior", 20, 20, 5, 10, 1, 0);
+            Console.WriteLine("Welcome to Arena! Enter the name of your warrior: ");
+
+            string name = Console.ReadLine();
+
+            Player Hero = new Player(0, name, 20, 20, 5, 10, 1, 0);
+
+            Console.WriteLine("Hello, {0}!", Hero.Name);
+
+            do
+            {
+                PrintMenu();
+
+                int action = int.Parse(Console.ReadLine()); // wczytaj numer polecenia
+                switch (action)
+                {
+                    case 0:
+                        Console.WriteLine("Shuting down.. ");
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        DifficultyMenu();
+                        break;
+                    case 2:
+                        Console.WriteLine("You have {0} experience points", Hero.ExperiencePoints);
+                        break;
+                }
+            }
+            while (true);
+
+
             Monster monster = new Monster(0, "Monster", 5, 5, 1, 5, 10);
 
-            Battle.StartFight(hero, monster);
+            Battle.StartFight(Hero, monster);
 
 
         }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine("Avalible actions:");
+            Console.WriteLine("\t0 - Quit");
+            Console.WriteLine("\t1 - Start fighting!");
+            Console.WriteLine("\t2 - Check the level of experience");
+            Console.Write("Choose action: ");
+        }
+
+        private static void DifficultyMenu()
+        {
+            Console.WriteLine("Choose a difficulty level:");
+            Console.WriteLine("\t1 - Easy");
+            Console.WriteLine("\t2 - Medium");
+            Console.WriteLine("\t3 - Hard");
+        }
     }
 }
+
+
---
+++ temp.cs
@@ -0,0 +1,55 @@
+﻿static void Main(string[] args)
+{
+    
+
+    Player newPlayer = new Player(0, name, 10, 10, 1, 0);
+
+    
+
+    newPlayer.Inventory.Add(new Weapon(World.ITEM_ID_RUSTY_SWORD, "Rusty sword", 5, 10));
+
+    Console.WriteLine("At the beginning you received a Rusty Sword!");
+
+    foreach (Weapon item in newPlayer.Inventory)
+    {
+        Console.WriteLine(item.Name);
+    }
+
+    
+}
+
+private static void PrintMenu()
+{
+    Console.WriteLine("Avalible actions:");
+    Console.WriteLine("\t0 - Quit");
+    Console.WriteLine("\t1 - Start fighting!");
+    Console.WriteLine("\t2 - Check your items");
+    Console.WriteLine("\t3 - Check the level of experience");
+    Console.WriteLine("\t4 - Choose a difficulty level");
+    Console.Write("Choose action: ");
+}
+
+
+
+    DifficultyLevel difficultyLevel = new SelectDifficultyLevel();
+
+
+
+
+
+
+    int action = int.Parse(Console.ReadLine()); // wczytaj numer polecenia
+    switch (action)
+    {
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            Console.WriteLine();
+            break;
+        case 3:
+            Console.WriteLine();
+            break;
+    }
+}
